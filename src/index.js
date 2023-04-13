ae4cb3ee88c41c593fddbc33b181eecdb2cc4ab1
JosiahSchwahn <schwahn.josiah@gmail.com>
2023-04-12 20:51:55 -0600

    .

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { project } from "./modules/project";
 
 const taskOne = taskItem("heasdlo")
 console.log(taskOne.getName());
+console.log(taskOne.getName());
 
 const projectOne = project("To do list");
 projectOne.addTask(taskOne);
